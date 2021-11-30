ae2c02a74ac96ac399269eb62bad3245ff11c9d2
wlals2997 <wlals2997@naver.com>
2021-11-30 22:25:01 +0900

    페이지만듬

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import MainPage from './pages/MainPage';
+import SelectPage from './pages/SelectPage';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/"  exact={true} element={<MainPage/>}/>
+        <Route path="/select" exact={true} element={<SelectPage/>}/>
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/SelectPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function SelectPage(){
+    return<div>
+        select페이지 입니다.
+    </div>
+}
+
+export default SelectPage;
