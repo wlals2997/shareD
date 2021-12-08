273ae96ce6bb580ea145f68cd85ce48a2bd211a9
wlals2997 <wlals2997@naver.com>
2021-12-08 21:43:47 +0900

    셀렉트페이지

--- src/pages/SelectPage.js
+++ src/pages/SelectPage.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
 function SelectPage(){
-    return<div>
-        select페이지 입니다.
+    return ( 
+    <div>
+    <div>
+        <div>
+            <h1>원하시는 OTT서비스를 선택하세요.</h1>
+           
+        </div>
+        <div>
+            <button>넷플릭스</button>
+            <button>웨이브</button>
+          
+        </div>
     </div>
+</div>
+    );
 }
 
 export default SelectPage;
