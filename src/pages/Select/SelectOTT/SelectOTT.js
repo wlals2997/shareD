e86331258703ec46021614ae1e8c8b934c6fcfef
wlals2997 <wlals2997@naver.com>
2021-12-22 15:46:44 +0900

    react-bootstrap style custom

--- src/pages/Select/SelectOTT/SelectOTT.js
+++ src/pages/Select/SelectOTT/SelectOTT.js
@@ -1,29 +1,61 @@
-import React from "react";
-import { Container,Row,Button } from "react-bootstrap";
-import {  useNavigate } from "react-router-dom";
-import  {ottService} from '../data'
-const SelectOTT=()=>{
-    const navigate = useNavigate();
+import React from 'react';
+import { Container, Row, Button, Card,Accordion } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import { ottService } from '../data';
+import SelectContainer from './SelectOTT.style';
 
-return(
-<div>
-<Container>
-  <Row>
-   {
-   Object.entries(ottService).map(function(ott,i){
-       console.log(ott)
-    //    let object=Object.entries(ottService);
-    //    console.log(object);
-          return (
-        <Button key={i} onClick={()=>{navigate('/select/date')}}>{ott[1].title}</Button>
-       )
-   })
+import styled from 'styled-components'
 
-   } 
-   
-  </Row>
-</Container>
-</div>
-)
-}
+
+
+const SelectOTT = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className='my-component'>
+    <Container>
+      <SelectContainer >
+        <Row>
+        <Card >
+  <Card.Body>
+            <Card.Title>보고싶은 서비스를 선택하세요</Card.Title>
+            {Object.entries(ottService).map(function (ott, i) {
+              console.log(ott);
+              //    let object=Object.entries(ottService);
+              //    console.log(object);
+              return (
+                <Button
+                  key={i}
+                  onClick={() => {
+                    navigate('/select/date');
+                  }}
+                >
+                  {ott[1].title}
+                </Button>
+              );
+            })}
+          </Card.Body>
+          </Card>
+        </Row>
+        
+        <Accordion>
+  <Accordion.Item eventKey="0">
+    <Accordion.Header>결제 후 환불이 가능한가요?</Accordion.Header>
+    <Accordion.Body>
+      환불하지마
+    </Accordion.Body>
+  </Accordion.Item>
+  <Accordion.Item eventKey="1">
+    <Accordion.Header>결제 후 어떻게 사용할 수 있나요?</Accordion.Header>
+    <Accordion.Body>
+      저희 쉐어디 마이페이지에서 아이디, 비밀번호를 확인하신 후 구독하신 OTT서비스를 통해 
+      로그인하시면 바로 사용가능합니다.
+    </Accordion.Body>
+  </Accordion.Item>
+</Accordion>
+      </SelectContainer>
+    </Container>
+    </div>
+  );
+};
 export default SelectOTT;
--- src/pages/Select/SelectOTT/SelectOTT.style.js
+++ src/pages/Select/SelectOTT/SelectOTT.style.js
@@ -2,17 +2,26 @@ import styled from 'styled-components';
 
 import * as BootStrap from 'react-bootstrap';
 
-const OTTCard = styled(BootStrap.Card)`
-  margin-top: 50px;
-  background: #f0f0f0;
-`;
-const Body = styled(BootStrap.Card.Body)`
-  background: red;
-`;
-const Title = styled(BootStrap.Card.Title)`
-  background: red;
-`;
 
-export default OTTCard;
 
-export { Title, Body };
+const SelectContainer= styled(BootStrap.Container)
+`background:white ;
+max-width:640px;
+width:100%;
+padding:10px 30px;
+margin-top:50px
+`;
+//  const SelectCard = styled(BootStrap.Card)`
+// //   margin-top: 50px;
+// //   background: #f0f0f0;
+// // `;
+// const Body = styled(BootStrap.Card.Body)`
+//   background: red;
+// `;
+ const SelectTitle = styled(BootStrap.Card.Title)`
+  font-weight:700;
+ `;
+
+export default SelectContainer;
+
+// export { Container };
