6fed81afb0a06e6cf08637d8bb649aba85dac2de
wlals2997 <wlals2997@naver.com>
2022-01-10 00:38:45 +0900

    메인페이지 버튼

--- src/pages/Main/MainPage.js
+++ src/pages/Main/MainPage.js
@@ -3,18 +3,20 @@ import styled from 'styled-components';
 import main_Image from '../../data/Saly-24.jpg';
 import { useNavigate } from 'react-router-dom';
 
-const Main = styled.div`
-  display: flex;
-
-  @media only screen and (max-width: 500px) {
-    margin-top: 130px;
-    height: 400px;
-    width: 530px;
-  }
-`;
+const Main=styled.div`
+display:flex;
+justify-content: center;
+align-items: center;
+`
 const MainImage = styled.img`
+
   width: 800px;
   margin: auto;
+  @media only screen and (max-width: 768px) {
+    max-width: 100%;
+    height: auto;
+    margin-top: 100px;
+  }
 `;
 const Startbtn = styled.button`
   background-color: #553ced;
@@ -23,18 +25,29 @@ const Startbtn = styled.button`
   border-radius: 8px;
   color: white;
 `;
+const Btn = styled.div`
+  @media only screen and (max-width: 768px) {
+    position: absolute;
+    top: 70%;
+    left: 15%;
+  }
+`;
 const MainPage = () => {
   const navigate = useNavigate();
   return (
     <Main>
-      <MainImage src={main_Image} alt='mainImage'></MainImage>
-      <Startbtn
-        onClick={() => {
-          navigate('/select');
-        }}
-      >
-        시작하기
-      </Startbtn>
+      <div>
+        <MainImage src={main_Image} alt='mainImage'></MainImage>
+      </div>
+      <Btn>
+        <Startbtn
+          onClick={() => {
+            navigate('/select');
+          }}
+        >
+          시작하기
+        </Startbtn>
+      </Btn>
     </Main>
   );
 };
